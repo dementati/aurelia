f2c3bbda43600809be6e82e93b4de742b4b21f8c
Lucas Lindstrom <ztargaser@gmail.com>
2013-11-03 14:37:15 +0100

    Settings activity and basic aurora checking functionality.

--- src/com/github/dementati/aurelia/GeophysInstRetriever.java
+++ src/com/github/dementati/aurelia/GeophysInstRetriever.java
@@ -1,6 +1,7 @@
 package com.github.dementati.aurelia;
 
 import java.io.IOException;
+import java.util.Calendar;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,11 +9,11 @@ import org.jsoup.select.Elements;
 
 import android.util.Log;
 
-public class GeophysInstRetriever {
-	public int retrieveLevel(String region, int year, int month, int day) {
-		Log.i("GeophysInstRetriever", "retrieveLevel(" + region + ", " + year + ", " + month + ", " + day + ")");
+public class GeophysInstRetriever {	
+	public static int retrieveLevel(int year, int month, int day) {
+		Log.i("GeophysInstRetriever", "retrieveLevel(" + year + ", " + month + ", " + day + ")");
 		
-		String url = "http://www.gi.alaska.edu/AuroraForecast/" + region + "/" + year + "/" + month + "/" + day;
+		String url = "http://www.gi.alaska.edu/AuroraForecast/Europe/" + year + "/" + month + "/" + day;
 		Document doc;
 		try {
 			doc = Jsoup.connect(url).get();
@@ -24,5 +25,14 @@ public class GeophysInstRetriever {
 		}
 		
 		return -1;
+	}
+	
+	public static int retrieveLevel() {
+		Calendar calendar = Calendar.getInstance();
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH) + 1;
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		
+		return retrieveLevel(year, month, day);
 	}
 }
--- src/com/github/dementati/aurelia/MainActivity.java
+++ src/com/github/dementati/aurelia/MainActivity.java
@@ -1,15 +1,17 @@
 package com.github.dementati.aurelia;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-	
-	UserState userState = new UserState();
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        
-        String url = "http://www.gi.alaska.edu/AuroraForecast/Europe/2013/11/02";
-        new DataRetrievalTask().execute(url);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -30,17 +28,61 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    private class DataRetrievalTask extends AsyncTask<String, Void, String> {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch(item.getItemId()) {
+    		case R.id.action_settings:
+    			openSettings();
+    			return true;
+    			
+    		default:
+    			return super.onOptionsItemSelected(item);
+    	}
+    }  
+    
+    @Override
+    protected void onResume() {
+    	// TODO Auto-generated method stub
+    	super.onResume();
+    	
+    	update();
+    }
+    
+    public void update() {
+    	TextView textView = (TextView)findViewById(R.id.output);
+    	textView.setText(R.string.output_neutral);
+		textView.setTextColor(getResources().getColor(R.color.neutral));
+    	
+    	new DataRetrievalTask().execute();
+    }
+    
+    public void openSettings() {
+    	Intent intent = new Intent(this, SettingsActivity.class);
+    	startActivity(intent);
+    }
+    
+    private class DataRetrievalTask extends AsyncTask<Void, Void, Boolean> {
     	@Override
-    	protected String doInBackground(String... urls) {
-    		return Integer.toString(userState.retrieveLevel());
+    	protected Boolean doInBackground(Void... params) {
+    		int currentLevel = GeophysInstRetriever.retrieveLevel();
+    		
+    		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+    		int minLevel = pref.getInt("pref_level", 2);
+    		
+    		return Boolean.valueOf(currentLevel >= minLevel);
     	}
     	
     	@Override
-    	protected void onPostExecute(String result) {
-    		TextView textView = (TextView)findViewById(R.id.hello_world);
-			textView.setText(result);
+    	protected void onPostExecute(Boolean result) {
+    		TextView textView = (TextView)findViewById(R.id.output);
+    		
+    		if(result.booleanValue()) {
+    			textView.setText(R.string.output_go);
+    			textView.setTextColor(getResources().getColor(R.color.go));
+    		} else {
+    			textView.setText(R.string.output_stay);
+    			textView.setTextColor(getResources().getColor(R.color.stay));
+    		}
     	}
     }
-    
 }
